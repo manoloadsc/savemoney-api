8fee63a1e21237ed7ef257365472a2c193404c56
motta-phenrique <motta.phenrique@gmail.com>
2025-10-01 14:51:11 -0300

    update plans

--- src/types/plans.ts
+++ src/types/plans.ts
@@ -1,12 +1,12 @@
 export const plans = [
   {
-    id: "8qihf6b2",
+    id: "v82fo0jb",
     name: "Plano Anual",
     description: "",
     durationDays: 365,
   },
   {
-    id: "wmjcx3hw",
+    id: "l7tsh9fz",
     name: "Plano Trimestral",
     description: "",
     durationDays: 90,
